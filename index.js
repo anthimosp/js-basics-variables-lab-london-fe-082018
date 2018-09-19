55e831066bce074adf71ebbac58f8bbf4a2e60e9
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-19 19:26:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
+describe('index.js', function () {
+  describe('companyName', function () {
+    it('is set as Scuber', function () {
+      expect(companyName).to.equal('Scuber');
+    });
+
+    it('raises error if the companyName is changed', function () {
+      expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+    });
+  });
